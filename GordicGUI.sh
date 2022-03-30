41d253fd7ba02c9a2f7a3da46d19964966a469c4
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2022-03-30 09:40:11 +0200

    add sound and printer redirection

--- GordicGUI.sh
+++ GordicGUI.sh
@@ -15,7 +15,7 @@ exit 0
 fi
 if [ "$?" -eq "0" ]
 then
-xfreerdp /gu:"$G_USERNAME" /u:"$G_USERNAME" /gp:"$G_PASSWORD" /p:"$G_PASSWORD" /kbd:"0x00000409" /gd:FPO /g:app.fpobk.cz /v:FPO-BRK.fpo.local /d:FPO /load-balance-info:"tsv://MS Terminal Services Plugin.1.ASI" /cert:ignore /app:"||wslgsql" /shell:"||wslgsql" /rfx +fonts +clipboard +compression
+xfreerdp /gu:"$G_USERNAME" /u:"$G_USERNAME" /gp:"$G_PASSWORD" /p:"$G_PASSWORD" /kbd:"0x00000409" /gd:FPO /g:app.fpobk.cz /v:FPO-BRK.fpo.local /d:FPO /load-balance-info:"tsv://MS Terminal Services Plugin.1.ASI" /cert:ignore /app:"||wslgsql" /shell:"||wslgsql" /rfx +fonts +clipboard +compression /sound /printer
 zenity --info \
 --text="Gordic Cloud was terminated"
 else
